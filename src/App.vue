c3301bb8ab0faf10d5451ca0529c03da462326bf
yasuaki640 <yasuaki640@gmail.com>
2023-05-23 14:50:28 +0900

    error

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,20 @@ type Belonging = {
   name: string;
 };
 
+const loading = ref(false);
+
 const belongings = ref<Belonging[]>([]);
+loading.value = true;
 fetch(import.meta.env.VITE_API_DOMAIN + "/belongings")
   .then((res) => res.json())
   .then((json) => {
     belongings.value = json;
+  })
+  .catch((e) => {
+    alert(e);
+  })
+  .finally(() => {
+    loading.value = false;
   });
 
 const removeBelonging = (id: string) => {
@@ -21,6 +30,10 @@ const removeBelonging = (id: string) => {
 const nameInput = ref("");
 
 const addBelonging = () => {
+  if (!nameInput.value) {
+    return;
+  }
+
   belongings.value.push({
     id: uuidv4(),
     name: nameInput.value
@@ -33,15 +46,18 @@ const addBelonging = () => {
   <main>
     <h1>Vue3 Composable Sample</h1>
     <h2>旅行の持ち物 一括編集</h2>
-    <ul>
-      <li v-for="belonging in belongings" :key="belonging.id">
-        {{ belonging.name }}
-        <button @click="removeBelonging(belonging.id)">remove</button>
-      </li>
-    </ul>
-    <div id="add-todo-pane">
-      <input v-model="nameInput" type="text" />
-      <button @click="addBelonging">add</button>
-    </div>
+    <section v-if="loading">Loading...</section>
+    <section v-else>
+      <ul>
+        <li v-for="belonging in belongings" :key="belonging.id">
+          {{ belonging.name }}
+          <button @click="removeBelonging(belonging.id)">remove</button>
+        </li>
+      </ul>
+      <div id="add-todo-pane">
+        <input v-model="nameInput" type="text" />
+        <button @click="addBelonging">add</button>
+      </div>
+    </section>
   </main>
 </template>
