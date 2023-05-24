61174c83b345aa85aa0661f3bc5a3dbe1a94f1b0
yasuaki640 <yasuaki640@gmail.com>
2023-05-24 10:37:36 +0900

    prepare vites

---
+++ src/hooks/useEditBelongings.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { useEditBelongings } from "@/hooks/useEditBelongings";
+
+describe("useEditBelongings", () => {
+  it("should be defined", () => {
+    expect(useEditBelongings).toBeDefined();
+  });
+});
